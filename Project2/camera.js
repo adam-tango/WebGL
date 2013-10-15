851547dac065918ec0a239167a5ff957abcf8398
TangoUCF <act4lmr@aol.com>
2013-10-14 21:59:18 -0400

    Updated camera - Works properly now
    Up to you guys but camera may be done. Only issue I saw was when rotating after move up,down,left,right,etc., the rotation doesn't occur around the model anymore. Rather a point just in front of the camera.

--- Project2/camera.js
+++ Project2/camera.js
@@ -29,13 +29,23 @@ function Camera(gl,d,modelUp) // Compute a camera from model's bounding box dime
 		return [at[0], at[1], at[2]];
 	}
 	
-	/*
-		ROTATIONS AND SHIFTS ASSUME UP VECTOR IS POSITIVE Y AXIS
-	*/
-	// PAN: rotate camera around up-axis (V-axis) - left, right
+	// Calculate adjusted u,v,w axis in world space
+	this.uAxis = function() {
+		return cross(up, this.wAxis());	
+	};
+	this.vAxis = function() {
+		return cross(this.wAxis(), this.uAxis());
+	};
+	this.wAxis = function() {
+		return normalize(subtractVectors(eye, at));	
+	};
+	
+	
+	// PAN: rotate camera around Y-axis (V-axis) - left, right
 	this.pan = function(direction){
 		var angle = (direction)?rotationAngle:rotationAngle*-1;	
-		var m = new Matrix4().setTranslate(at[0],at[1],at[2]).rotate(-angle,up[0],up[1],up[2]);
+		var V = this.vAxis();
+		var m = new Matrix4().setTranslate(at[0],at[1],at[2]).rotate(-angle,V[0],V[1],V[2]);
 		m.translate(-at[0],-at[1],-at[2]);
 		var e = m.multiplyVector4(new Vector4([eye[0],eye[1],eye[2],1])).elements;		
 		eye[0]=e[0]; eye[1]=e[1]; eye[2]=e[2];
@@ -45,7 +55,8 @@ function Camera(gl,d,modelUp) // Compute a camera from model's bounding box dime
 	this.tilt = function(direction) { 
 		var angle = (direction)?rotationAngle:rotationAngle*-1;	
 		var step = eye[2];
-		var m = new Matrix4().setTranslate(at[0],at[1],at[2]).rotate(-angle,1,0,0).translate(-at[0],-at[1],-at[2])
+		var U = this.uAxis();
+		var m = new Matrix4().setTranslate(at[0],at[1],at[2]).rotate(-angle,U[0],U[1],U[2]).translate(-at[0],-at[1],-at[2])
 		var e = m.multiplyVector4(new Vector4([eye[0],eye[1],eye[2],1])).elements;
 		step = Math.abs(step - e[2]);
 		if(e[2] > step || e[2] < step*-1) { // prevent crossing up vector
@@ -61,42 +72,61 @@ function Camera(gl,d,modelUp) // Compute a camera from model's bounding box dime
 		if(FOV==180) FOV--;
 	};
 
-	// DOLLY: Move camera closer or farther back
+	// DOLLY: Move camera closer or farther back (W axis)
 	this.dolly = function(direction) { 
 		var d = (direction)?delta:delta*-1;
-	
-		var m = new Matrix4().setTranslate(0, 0, -d);
+		var W = this.wAxis();
+		var m = new Matrix4().setTranslate(W[0]*-d, W[1]*-d, W[2]*-d);
 		var e = m.multiplyVector4(new Vector4([eye[0],eye[1],eye[2],1])).elements;
 		eye[0]=e[0]; eye[1]=e[1]; eye[2]=e[2];
-		
-		var d = new Matrix4().setTranslate(0, 0, -d);
+		var d = new Matrix4().setTranslate(W[0]*-d, W[1]*-d, W[2]*-d);
 		var a = d.multiplyVector4(new Vector4([at[0],at[1],at[2],1])).elements;
 		at[0]=a[0]; at[1]=a[1]; at[2]=a[2];
 	};
 	
-	// PEDESTAL: Move camera up or down
+	// PEDESTAL: Move camera up or down (V axis)
 	this.pedestal = function(direction) { 
 		var d = (direction)?delta:delta*-1;
-	
-		var m = new Matrix4().setTranslate(up[0]*d, up[1]*d, up[2]*d);
+		var V = this.vAxis();
+		var m = new Matrix4().setTranslate(V[0]*d, V[1]*d, V[2]*d);
 		var e = m.multiplyVector4(new Vector4([eye[0],eye[1],eye[2],1])).elements;
 		eye[0]=e[0]; eye[1]=e[1]; eye[2]=e[2];
-		
-		var d = new Matrix4().setTranslate(up[0]*d, up[1]*d, up[2]*d);
+		var d = new Matrix4().setTranslate(V[0]*d, V[1]*d, V[2]*d);
 		var a = d.multiplyVector4(new Vector4([at[0],at[1],at[2],1])).elements;
 		at[0]=a[0]; at[1]=a[1]; at[2]=a[2];
 	};
 	
-	// TRUCK: Move camera left or right  
+	// TRUCK: Move camera left or right  (U axis)
 	this.truck = function(direction) { 
 		var d = (direction)?delta:delta*-1;
-	
-		var m = new Matrix4().setTranslate(-d, 0, 0);
+		var U = this.uAxis();
+		var m = new Matrix4().setTranslate(U[0]*-d, 0, 0);
 		var e = m.multiplyVector4(new Vector4([eye[0],eye[1],eye[2],1])).elements;
 		eye[0]=e[0]; eye[1]=e[1]; eye[2]=e[2];
 		
-		var d = new Matrix4().setTranslate(-d, 0, 0);
+		var d = new Matrix4().setTranslate(U[0]*-d, 0, 0);
 		var a = d.multiplyVector4(new Vector4([at[0],at[1],at[2],1])).elements;
 		at[0]=a[0]; at[1]=a[1]; at[2]=a[2];
 	};
+}
+
+// vector math functions
+function normalize(v) {
+  var length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+  // make sure we don't divide by 0.
+  if (length > 0.00001) {
+    return [v[0] / length, v[1] / length, v[2] / length];
+  } else {
+    return [0, 0, 0];
+  }
+}
+
+function cross(a, b) {
+  return [a[1] * b[2] - a[2] * b[1],
+          a[2] * b[0] - a[0] * b[2],
+          a[0] * b[1] - a[1] * b[0]];
+}
+
+function subtractVectors(a, b) {
+  return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
 }
