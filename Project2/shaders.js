2b873e0a6f8b90a925f7052ad7bb7361637c31ca
TangoUCF <act4lmr@aol.com>
2013-10-16 19:11:44 -0400

    Update shaders.js
    Added uniform variables and made adjustment so omni light works better. Brighter like it should be. Spot light still needs work.

--- Project2/shaders.js
+++ Project2/shaders.js
@@ -1,9 +1,12 @@
 var VSHADER_SOURCE = 
 	'attribute vec3 aPosition;\n' +
 	'attribute vec3 aNormal;\n' +
-	'uniform vec3 uEyePosition; \n' +
-	'uniform vec3 uSceneCenter;\n' +
-	'uniform float uLightType;\n' +
+	
+	'uniform int uLightType;\n' +
+	'uniform vec3 uLightColor;\n' +
+	'uniform vec3 uEyePosition;\n' +
+	'uniform vec3 uSceneAmbient;\n' +
+	'uniform vec3 uLookAt;\n' +
 	
 	'uniform mat4 modelT, viewT, projT;\n'+
 	'uniform mat4 normalMatrix;\n' +
@@ -21,39 +24,30 @@ var VSHADER_SOURCE =
 		// ----------
 		// materials.emissionColor = [0,0,0,1]
 	'	vec3 emission = vec3(0,0,0);\n' +
-		// scence ambience
-	'	vec3 globalAmbience = vec3(0.1,0.1,0.1);\n' + 	
 		// materials.ambientReflectance = [0,0,0,1] /*/ materials all have 0 ambienceReflectance, don't multiply
-	'	vec3 ambience = globalAmbience;\n' + // vec3 ambience = vec3(0,0,0) * globalAmbience;\n' +
+	'	vec3 ambience = uSceneAmbient;\n' + // vec3 ambience = vec3(0,0,0) * uSceneAmbience;\n' +
 
 		// ----------
 		// DIFFUSE
 		// ----------
-	'	vec3 L;\n' +
-	'	if(uLightType == 1.0) {\n' +
-			// point light
-	'		L = normalize(uEyePosition-position);\n' + 
-    '	} else {\n' +
-			// spot light
-			// vector same for all light rays - not sure this is correct
-    '		L = normalize(uEyePosition-uSceneCenter);\n' +
+	'	vec3 L = normalize(uEyePosition-position);\n' + 
+	
+	'   float diffuseLight = max(dot(normal,L), 0.0);\n' +
+	'	if(uLightType == 0) {\n' +
+	'		diffuseLight = max(dot(normal,-L), 0.0);\n' + // spotlight requires -L
 	'	}\n' +
-		
-	'	float diffuseLight = max(dot(normal,L), 0.0);\n' +
+	
 		// kd = materials.diffuseReflectance = [0.6,0.6,0.6,1]
 	'	vec3 Kd = vec3(0.6,0.6,0.6);\n' +
-		// lightColor = white
-	'	vec3 lightColor = vec3(1,1,1);\n' +
-	'	vec3 diffuse = Kd*lightColor*diffuseLight;\n' +
+	'	vec3 diffuse = Kd*uLightColor*diffuseLight;\n' +
 
 		// ----------
 		// SPECULAR
 		// ----------
-		// differs per vertex whether point or omni source
-	'	vec3 V = normalize(uEyePosition - position);\n' +
+	'	vec3 V = L;\n' +
 	'	vec3 H = normalize(L + V);\n' +
 		// materials.shininess = 10
-	'	float specularPower = 100.0;\n' +
+	'	float specularPower = 10.0;\n' +
 		// calculate
 	'	float specularLight = pow(dot(normal,H),specularPower);\n' + 
 		// is it possible to have specular lighting
@@ -61,19 +55,19 @@ var VSHADER_SOURCE =
 		// materials.specularReflectance = [0.4,0.4,0.4,1]
 	'	vec3 Ks = vec3(0.4,0.4,0.4);\n' +
 		// calculate
-	'	vec3 specular = Ks*lightColor*specularLight;\n' +	   
+	'	vec3 specular = Ks*uLightColor*specularLight;\n' +	   
 
 		// LIGHT
-	'	vec3 light;\n' +
-	'	if(uLightType == 1.0) {\n' +
-			// point light
-	'		light = emission + ambience + diffuse + specular;\n' + 
-	'	} else {\n' +
+	'	vec3 light = emission + ambience + diffuse + specular;\n' + // point light
+	'	if(uLightType == 0) {\n' +
 			// spot light
-	'		vec3 lightDirection = normalize(uEyePosition - position);\n' +
-	'		float spotPower = 10.0;\n' + // <-- not sure what this is
-	'		float spotScale = pow(max(dot(L,-lightDirection),0.0),spotPower);\n' +
-	'		light = emission + ambience + (diffuse + specular)*spotScale;\n' +
+	'		vec3 spotDirection = normalize( uEyePosition - uLookAt);\n' +
+	'		float angle = max(dot(normal, -L),0.0); \n' +
+	'		if(acos(angle) < radians(30.0))\n' +
+	'			light = emission + ambience + diffuse + specular;\n' +
+	'		else {\n' +
+	'			light = emission + ambience;\n' +
+	'		}\n' +
 	'	}\n' +
 		
 		// original mesh color (set to red for now) ; change to texture
