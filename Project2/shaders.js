5dca3bfd84c23d43bd8402c6209f04fdae5afc35
TangoUCF <act4lmr@aol.com>
2013-10-16 23:26:35 -0400

    Updated shades. We have a spotlight!

--- Project2/shaders.js
+++ Project2/shaders.js
@@ -2,80 +2,18 @@ var VSHADER_SOURCE =
 	'attribute vec3 aPosition;\n' +
 	'attribute vec3 aNormal;\n' +
 	
-	'uniform int uLightType;\n' +
-	'uniform vec3 uLightColor;\n' +
-	'uniform vec3 uEyePosition;\n' +
-	'uniform vec3 uSceneAmbient;\n' +
-	'uniform vec3 uLookAt;\n' +
-	
 	'uniform mat4 modelT, viewT, projT;\n'+
 	'uniform mat4 normalMatrix;\n' +
 	
-	'varying vec3 vColor;\n' +
+	'varying vec3 vPosition;\n' +
+	'varying vec3 vNormal;\n' +
 	
 	'void main() {\n' +
 		
 	'   gl_Position = projT*viewT*modelT*vec4(aPosition,1.0);\n' +
-	'   vec3 position = vec3(normalMatrix * vec4(aPosition, 1.0));\n' +
-    '   vec3 normal = normalize(vec3(normalMatrix * vec4(aNormal, 1.0)));\n' +
-
-		// ----------
-		// Emissive and ambient light
-		// ----------
-		// materials.emissionColor = [0,0,0,1]
-	'	vec3 emission = vec3(0,0,0);\n' +
-		// materials.ambientReflectance = [0,0,0,1] /*/ materials all have 0 ambienceReflectance, don't multiply
-	'	vec3 ambience = uSceneAmbient;\n' + // vec3 ambience = vec3(0,0,0) * uSceneAmbience;\n' +
-
-		// ----------
-		// DIFFUSE
-		// ----------
-	'	vec3 L = normalize(uEyePosition-position);\n' + 
+	'	vPosition = vec3(viewT*modelT*vec4(aPosition,1.0));\n' +
+   	'	vNormal = normalize(vec3(viewT*normalMatrix*vec4(aNormal,0.0)));\n' +
 	
-	'   float diffuseLight = max(dot(normal,L), 0.0);\n' +
-	'	if(uLightType == 0) {\n' +
-	'		diffuseLight = max(dot(normal,-L), 0.0);\n' + // spotlight requires -L
-	'	}\n' +
-	
-		// kd = materials.diffuseReflectance = [0.6,0.6,0.6,1]
-	'	vec3 Kd = vec3(0.6,0.6,0.6);\n' +
-	'	vec3 diffuse = Kd*uLightColor*diffuseLight;\n' +
-
-		// ----------
-		// SPECULAR
-		// ----------
-	'	vec3 V = L;\n' +
-	'	vec3 H = normalize(L + V);\n' +
-		// materials.shininess = 10
-	'	float specularPower = 10.0;\n' +
-		// calculate
-	'	float specularLight = pow(dot(normal,H),specularPower);\n' + 
-		// is it possible to have specular lighting
-	'	if(diffuseLight<=0.0) specularLight=0.0;\n' +
-		// materials.specularReflectance = [0.4,0.4,0.4,1]
-	'	vec3 Ks = vec3(0.4,0.4,0.4);\n' +
-		// calculate
-	'	vec3 specular = Ks*uLightColor*specularLight;\n' +	   
-
-		// LIGHT
-	'	vec3 light = emission + ambience + diffuse + specular;\n' + // point light
-	'	if(uLightType == 0) {\n' +
-			// spot light
-	'		vec3 spotDirection = normalize( uEyePosition - uLookAt);\n' +
-	'		float angle = max(dot(normal, -L),0.0); \n' +
-	'		if(acos(angle) < radians(30.0))\n' +
-	'			light = emission + ambience + diffuse + specular;\n' +
-	'		else {\n' +
-	'			light = emission + ambience;\n' +
-	'		}\n' +
-	'	}\n' +
-		
-		// original mesh color (set to red for now) ; change to texture
-	'	vec3 color = vec3(1,0,0);\n' +
-		// modify color by the cumulative light hitting it
-	'	color.rgb *= light;\n' +
-		// pass to fragment shader
-	'   vColor = color;\n' +
 	'}\n';
 	
 	
@@ -83,7 +21,72 @@ var FSHADER_SOURCE =
 	'#ifdef GL_ES\n' +
   	'	precision highp float;\n' +
     '#endif\n' +
-	'	varying vec3 vColor;\n' + 
+	
+	'	varying vec3 vPosition;\n' +
+	'	varying vec3 vNormal;\n' +
+	
+	'	uniform int uLightType;\n' +
+	'	uniform vec3 uEyePosition;\n' +
+	'   uniform vec3 uLightColor;\n' +
+	'	uniform vec3 uSceneAmbient;\n' +
+	
     '	void main() {\n' +
-	'     gl_FragColor = vec4(vColor, 1.0);\n' +	  
+			
+	'		vec3 color = vec3(1,0,0);\n' +			// make models red - texturize in the future
+	'		vec3 normal = normalize(vNormal);\n' +	
+		
+	// EMISSIVE REFLECTANCE:
+			// materials.emissionColor = [0,0,0,1]
+	'		vec3 emission = vec3(0,0,0);\n' +
+
+	
+	// AMBIENT REFLECTANCE
+			// materials.ambientReflectance = [0,0,0,1] 
+			// chose not to multiply the two because the materials cause no ambient reflection
+	'		vec3 ambience = uSceneAmbient;\n' + // vec3 ambience = vec3(0,0,0) * uSceneAmbience;\n' +
+
+
+	// DIFFUSE REFLECTANCE:
+			// direction from eye (camera location) to fragment
+	'		vec3 lightDirection = normalize(uEyePosition-vPosition);\n' +	
+			// The dot product of the light direction and the normal
+	'		float nDotL = max(dot(lightDirection,normal), 0.0);\n' +	
+			// calculate diffuse reflectance
+	'		vec3 diffuse = uLightColor * vec3(0.6,0.6,0.6) * nDotL;\n' +	
+
+
+	// SPECULAR REFLECTANCE:
+   			// make spotlight always hit center of the screen (vec3(0,0,0))
+	'		vec3 viewDirection = normalize(-vPosition);\n' +	
+			// get reflection vector
+	'		vec3 reflectDirection = reflect(-lightDirection,normal);\n' +	
+	  		// angle between view and reflection 
+	'		float vDotR = max(dot(viewDirection, reflectDirection),0.0);\n' +	
+			// calculate specular reflectance (10.0 => material.shininess; pow() restricts angle)
+	'		vec3 specular = uLightColor * vec3(0.4,0.4,0.4) * pow(vDotR, 10.0);\n' +	
+			
+			// direction from camera space; since light comes from camera
+	'		vec3 spotlightDirection = vec3(0.0,0.0,-1.0);\n' +	
+   			// Calculate angle between spot-light look direction and from light to
+	'		float sDotF = dot(spotlightDirection,-viewDirection);\n' +	
+	
+	
+	// DETERMINE LIGHT COLOR BASED ON COMPONENTS:
+			// assumes omni light
+	'		vec3 tColor = vec3(emission + ambience + diffuse + specular);\n' +
+			// alter color if source is spotlight
+	'		if(uLightType == 0) {\n' +
+   				// calculate cone of spotlight
+	'			float spotDifference = 0.0;\n' +
+				// allow 10 degree spotlight angle
+	'			if(acos(sDotF) < radians(10.0)) {\n' +
+					// 60.0 restricts angle; determines how spotlight fades out (domain: 0.0 - 120.0)
+	'				spotDifference = pow(sDotF, 60.0);\n' +	 
+	'			}\n' +
+				// emmission & ambience are not restricted by spotlight
+	'			tColor = vec3(emission + ambience + spotDifference * (diffuse + specular));\n' +		
+	'		}\n' + 
+	
+	'    	gl_FragColor = vec4(color*tColor, 1.0);\n' +	  
+	
 	'  }\n';
